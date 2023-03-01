be8fbfc50aea7f9aa087f3586d86183c370dd9bc
hildeheggstad <hilde.steinbru.heggstad@coop.no>
2023-03-01 17:50:40 +0100

    gh pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          EN HELT NY APP
         </a>
       </header>
     </div>
