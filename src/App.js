401e8fb5e1c486badc9793dbf2007b0e7b9c69f5
hildeheggstad <hilde.steinbru.heggstad@coop.no>
2023-03-05 17:55:51 +0100

    mobilversjon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import nanna1 from './bilder/nanna11.jpg';
+import nanna2 from './bilder/nanna2.jpg';
+import nanna3 from './bilder/nanna3.jpg';
+import mona from './bilder/mona.png';
 
 function App() {
   return (
@@ -6,7 +10,10 @@ function App() {
         <h2 className={"overskrift"}>
           Auksjon
         </h2>
-          EN HELT NY APP
+            <img  className={"auksjon_bilde"} src={nanna1}></img>
+            <img  className={"auksjon_bilde"} src={mona}></img>
+            <img className={"auksjon_bilde"} src={nanna2}></img>
+            <img className={"auksjon_bilde"} src={nanna3}></img>
     </div>
   );
 }
